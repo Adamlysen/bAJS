f3e885cad1ac3605e1bdc37347b74e4b60350403
Adiladis <adam.lysen@elev.ga.ntig.se>
2023-08-31 10:28:49 +0200

    horunge

---
+++ ksdsdmsdsd.cs
@@ -0,0 +1,14 @@
+
+
+
+Console.WriteLine("Vad heter doo?");
+string bajs = Console.ReadLine();
+
+Console.WriteLine("HETER DO ROPENIS???");
+string name = Console.ReadLine();
+
+Console.if (name = "Ja");
+
+{
+    Console.WriteLine("FUCKING HORUNGEEEEEEEEE");
+}
